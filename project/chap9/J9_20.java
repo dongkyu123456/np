da1bcca0231b1f5374411049b6bf6b41eaea0f84
dongkyu123456 <121007226+dongkyu123456@users.noreply.github.com>
2023-03-20 17:51:07 +0900

    챕터8완료, 챕터9진행중

---
+++ project/chap9/J9_20.java
@@ -0,0 +1,11 @@
+package project.chap9;
+
+//page 346
+public class J9_20 {
+    public static void main(String[] args) {
+        StringBuffer sb = new StringBuffer("01");
+        StringBuffer sb2 = sb.append(23);
+        sb.append('4').append(56);
+    }
+}
+ㅁㄴㅇㄹㄴㄹㅇ
